df7b8da81a644df8129fad0994dcad02eb090dd5
Yoav Hirshberg <yoav.yh@gmail.com>
2021-08-20 17:19:32 +0300

    changing fn name

--- src/components/Items.tsx
+++ src/components/Items.tsx
@@ -4,20 +4,20 @@ import { ItemsContext } from '../store/items-context';
 
 import classes from './Items.module.css';
 
-const Todos: React.FC = () => {
-  const todosCtx = useContext(ItemsContext);
+const Items: React.FC = () => {
+  const itemsCtx = useContext(ItemsContext);
 
   return (
     <ul className={classes.items}>
-      {todosCtx.items.map(item => (
+      {itemsCtx.items.map(item => (
         <ListItem
           key={item.id}
           text={item.text}
-          onRemoveItem={todosCtx.removeItem.bind(null, item.id)}
+          onRemoveItem={itemsCtx.removeItem.bind(null, item.id)}
         />
       ))}
     </ul>
   );
 };
 
-export default Todos;
+export default Items;
